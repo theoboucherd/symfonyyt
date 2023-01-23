7bb56c67324522ecdc63102206b589f06069ab16
theoboucherd <theo.boucher017@gmail.com>
2023-01-23 10:50:41 +0100

    Devmusc

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recette', name: 'recipe.index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Generator;
+use App\Entity\Ingredient;
+use App\Entity\Recipe;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{  /** 
+   * @var Generator
+    */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    //ingredients
+    {
+        $ingredients = [];
+        for ($i=0; $i < 50; $i++) { 
+            $ingredient = new Ingredient ();
+            $ingredient->setName($this->faker->word())
+            ->setPrice(mt_rand(0, 100));
+        $ingredients[] = $ingredient;
+        $manager->persist($ingredient);
+        }
+
+        //recipes
+        for ($j =0; $j < 25; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setdifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                ->setDescription($this->faker->text(300))
+                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);   
+                
+                for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                    $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) -1 )]);                      
+                }
+
+                $manager->persist($recipe);
+        }
+        
+
+
+        $manager->flush();
+    }
+}
